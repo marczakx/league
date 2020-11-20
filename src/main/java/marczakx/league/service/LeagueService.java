a3fe63c7b42cef6cf4dc17c8e02887141e4b6757
tom <marczakx@gmail.com>
2020-11-20 08:33:31 +0100

    [multi] Created multi

--- src/main/java/marczakx/league/service/LeagueService.java
+++ src/main/java/marczakx/league/service/LeagueService.java
@@ -1,9 +1,14 @@
 package marczakx.league.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import marczakx.league.model.League;
 import marczakx.league.model.Match;
 import marczakx.league.model.Team;
+import marczakx.league.repository.LeagueRepository;
 import marczakx.league.repository.MatchRepository;
 import marczakx.league.repository.TeamRepository;
 import marczakx.league.service.leagueCreator.LeagueTableEntry;
@@ -15,10 +20,24 @@ public class LeagueService {
 	private MatchRepository matchRepository;
 	@Autowired
 	private TeamRepository teamRepository;
+	@Autowired
+	private LeagueRepository leagueRepository;
 
 	public Iterable<LeagueTableEntry> listLeague() {
 		Iterable<Match> matches = matchRepository.findAll();
 		Iterable<Team> teams = teamRepository.findAll();
+		return LeagueCreator.builder().teams(teams).matches(matches).build().create();
+	}
+	
+	public Iterable<LeagueTableEntry> listLeague(Long id) {
+
+		Optional<League> optional = leagueRepository.findById(id);
+		if(optional.isEmpty()) {
+			return null;
+		}
+		League league = optional.get();
+		Iterable<Match> matches = league.getMatches();
+		Iterable<Team> teams = league.getTeams();
 		return LeagueCreator.builder().teams(teams).matches(matches).build().create();
 	}
 }
